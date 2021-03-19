8bdef2cfecdb50c37ea18dfaee0d7b081a4174e9
Usuario generico alumno <alumno@ubuntu2004.localdomain>
2021-03-19 12:54:38 +0100

    añadido comentarios y mejoras del codigo

--- contentapp.py
+++ contentapp.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
 """
- contentApp class
- Simple web application for managing content
+ contentPostApp
+ Simple web application for managing content: get and post.
 
- Copyright Jesus M. Gonzalez-Barahona, Gregorio Robles 2009-2015
- jgb, grex @ gsyc.es
- TSAI, SAT and SARO subjects (Universidad Rey Juan Carlos)
- October 2009 - March 2015
+ Author: César Borao CoratinosJesus
+ Date: March 2021
+
+ Based on "webapp class" and contentapp.py, developed by M. Gonzalez-Barahona
+ & Gregorio Robles 2009-2015 (Universidad Rey Juan Carlos)
+
 """
 
 import webapp
@@ -16,24 +18,27 @@ import webapp
 class contentApp (webapp.webApp):
     """Simple web application for managing content.
 
-    Content is stored in a dictionary, which is intialized
-    with the web content."""
+    Content is stored in a dictionary, which is initialized
+    with the web content.
+    The client can accest the content or add new content"""
 
-    # Declare and initialize content
+    # Declare and initialize initial content
     content: dict = {'/': 'Root page',
-               '/page': 'A page'
                }
 
     def parse(self, request):
-        """Return the resource name (including /)"""
+        """Return:
+            1) the method of the request (POST or GET)
+            2) the resource name (including /)
+            3) the body of the request
+        """
 
         method = request.split(' ', 2)[0]
-        #print(method)
         resource = request.split(' ', 2)[1]
-        #print(resource)
+
+        # We locate the start of the request body
         index = request.find('\r\n\r\n')
         body = request[index:]
-        #print(body)
 
         return method, resource, body
 
@@ -41,27 +46,37 @@ class contentApp (webapp.webApp):
 
         (method, resource, body) = parsedRequest
 
-        print (self.content)
+        # print (self.content)
         if method == "GET":
+            # if the resource is in the dictionary
             if resource in self.content:
                 httpCode = "200 OK"
-                htmlBody = "<html><body>" + self.content[resource] \
+                htmlBody = "<html><body> Your are in " + self.content[resource] \
                     + "</body></html>"
+            # if the resource is not in the dictionary
             else:
-                httpCode = "404 Not Found"
-                htmlBody = "Not Found"
 
+                httpCode = "404 Not Found"
+                htmlBody = "page " + resource + " not found"
+
+        # if it is a POST request, we want to add content to the dictionary
         if method == "POST":
+            # we seach the new resouce inside the body
             idx = body.find('newcontent')
             parsedbody = body[idx:]
-            info = parsedbody.split('=')[1]
-            print("El nuevo recurso es: " + info)
-            info =  '/'+ info
-            self.content[info] = info
+
+            # info is the new resource we want to add to our dictionary
+            # notice that we always add the '/' before (we suposse that the user
+            # always type and submit the resource to add without the '/')
+
+            info = '/' + parsedbody.split('=')[1]
+
+            # add the new resource into the dictionary
+            self.content[info] = info + ' page'
 
             httpCode = "200 OK"
-            htmlBody = "<html><body> Resource " + self.content[info] \
-                       + " added</body></html>"
+            htmlBody = "<html><body>" + self.content[info] \
+                       + " added to the dictionary</body></html>"
 
         return (httpCode, htmlBody)
 
